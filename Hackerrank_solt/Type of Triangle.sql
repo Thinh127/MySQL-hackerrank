f01512e1813a30bb5676e9e6b944d9299dc5f40b
Pham Huynh Truong Thinh <thinh7174@gmail.com>
2021-07-26 10:34:06 +0700

    Update Type of Triangle.sql

--- Hackerrank_solt/Type of Triangle.sql
+++ Hackerrank_solt/Type of Triangle.sql
@@ -1,2 +1,8 @@
-select IF(,
-				,)
+select IF(t.A + t.B > t.C,
+	 IF((t.A = t.B) and (t.B = t.C),
+	    "Equilateral",
+	   IF((t.A = t.B) or (t.B = t.C) or (t.C = t.A),
+	      "Isosceles",
+	     "Scalene"))
+	 "Not A Triangle")
+from TRIANGLES t;
