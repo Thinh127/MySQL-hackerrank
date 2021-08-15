74c57e216073580645842526fcdd815683e70bd9
Pham Huynh Truong Thinh <thinh7174@gmail.com>
2021-08-15 21:52:09 +0700

    Update The PADS.sql

--- Hackerrank_solt/The PADS.sql
+++ Hackerrank_solt/The PADS.sql
@@ -1,1 +1,7 @@
-
+select concat(o.Name, "(", subtr(o.Occupation, 1, 1), ")")
+from Occupations o
+order by o.Name;
+select concat("There is a total of ", count(o.Occupation), " ", lower(o.Occupation),".s")
+from Occupations o
+group by o.Occupation
+order by count(o.Occupation) asc;
